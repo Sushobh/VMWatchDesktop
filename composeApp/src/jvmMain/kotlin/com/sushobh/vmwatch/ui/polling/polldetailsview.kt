eeac0628d5e269aebac0c892944eaa18b09921d9
Sushobh <aryansushobh@gmail.com>
2025-08-14 08:25:07 +0530

    ApiConnection

--- composeApp/src/jvmMain/kotlin/com/sushobh/vmwatch/ui/polling/polldetailsview.kt
+++ composeApp/src/jvmMain/kotlin/com/sushobh/vmwatch/ui/polling/polldetailsview.kt
@@ -1,21 +1,40 @@
 package com.sushobh.vmwatch.ui.polling
 
-import androidx.compose.material3.VerticalDivider
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.sushobh.vmwatch.ui.ViewModelDetails
-import com.sushobh.vmwatch.ui.ViewModelList
 
 @Composable
 fun FLPollingDetailsView(pollingViewModel: PollingViewModel) {
     val connectionState = pollingViewModel.connectionState.collectAsState()
-    when(connectionState.value){
-        PollingVMConnectionState.Connected -> {
+    Column(modifier = Modifier.fillMaxSize()) {
+        when (connectionState.value) {
+            PollingVMConnectionState.Connected -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(
+                        "Connection successfull!",
+                        style = MaterialTheme.typography.h3,
+                        color = MaterialTheme.colors.primary
+                    )
+                }
+            }
 
-        }
-        PollingVMConnectionState.NotConnected -> {
-
+            PollingVMConnectionState.NotConnected -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(
+                        "Not connected, please check adb connection.",
+                        style = MaterialTheme.typography.h3,
+                        color = MaterialTheme.colors.error
+                    )
+                }
+            }
         }
     }
+
 }
